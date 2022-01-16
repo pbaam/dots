e93d0ec50ee34cafe4359ce72ed30b9eb816e277
5paam <paam1655@gmail.com>
2022-01-16 16:48:02 +0100

    Makefile rename and fix typo
    The same amount of memory is asigned to both RAM and disk

--- setups/qemu/config.mk
+++ setups/qemu/config.mk
@@ -7,5 +7,5 @@ BIOS =  $(UNTRACK-PREFIX)bios
 BIOS-URL = "https://github.com/clearlinux/common/raw/master/OVMF.fd"
 ISO-URL = "https://bouncer.gentoo.org/fetch/root/all/releases/amd64/autobuilds/20220102T170545Z/install-amd64-minimal-20220102T170545Z.iso"
 
-DISK-SPACE = 5G
-RAM-SPACE = 1G
+DISK-SIZE = 5G
+RAM-SIZE = 1G
